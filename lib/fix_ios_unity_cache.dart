c81229448bfdf812ef2b68a7670180582831a390
josercc <josercc@163.com>
2025-03-31 16:47:28 +0800

    [Change] 修复导出unity iOS会在新版本的CLR报错

--- lib/fix_ios_unity_cache.dart
+++ lib/fix_ios_unity_cache.dart
@@ -32,8 +32,7 @@ class FixIosUnityCache {
     loggerSuccess('修复iOS不支持BitCode完毕!');
 
     if (!await Directory(iosBuidDir).exists()) {
-      loggerError('$iosBuidDir路径不存在,请先通过Unity导出包!');
-      return false;
+      return true;
     }
 
     /// 删除之前的缓存
