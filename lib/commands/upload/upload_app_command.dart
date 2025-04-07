136c32487bc5d15d8bd88f63d559b08593c9cd51
josercc <josercc@163.com>
2025-04-07 16:50:01 +0800

    [Fix] 修复了显示对应平台的分支提示在最后的问题

--- lib/commands/upload/upload_app_command.dart
+++ lib/commands/upload/upload_app_command.dart
@@ -231,14 +231,16 @@ $changeLog
     if (environment.upload) {
       loggerDebug('开始上传ipa/apk......');
       final uploadLog =
-          '[Tag:${environment.tag}][Flutter(${environment.branch})][Unity(${environment.unityBranchName})]  新版本发布了，请下载体验!';
+          '[Tag:${environment.tag}][Flutter(${environment.branch})][Unity(${environment.unityBranchName})] ';
       String platformBranch = switch (environment.platform) {
         'ios' => '[IOS:${environment.iosBranch}]',
         'android' => '[Android:${environment.androidBranch}]',
         _ => '',
       };
       await uploadApp(
-          log: '$uploadLog $platformBranch', environment: environment);
+        log: '$uploadLog $platformBranch 新版本发布了，请下载体验!',
+        environment: environment,
+      );
     }
 
     if (environment.sendLog) {
