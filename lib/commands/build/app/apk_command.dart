9b8cb36ba339cd94f5b6b04a21d9a68eafd9d338
josercc <josercc@163.com>
2026-01-16 14:43:45 +0800

    fix: 更新ApkCommand以使用绝对路径引用gradlew文件
    在ApkCommand中，修改了gradlew文件的引用方式，确保在构建命令中使用绝对路径。这一更改提高了构建过程的可靠性和兼容性。

--- lib/commands/build/app/apk_command.dart
+++ lib/commands/build/app/apk_command.dart
@@ -27,7 +27,7 @@ class ApkCommand extends Command {
 
     /// ./gradlew assembleRelease
     await ProcessRunner().runProcess(
-      ["./$gradlewName", 'assembleRelease'],
+      [gradlew.absolute.path, 'assembleRelease'],
       workingDirectory: androidDir,
       printOutput: true,
     );
