e03bc921c23884009fbf86ec96fcf3153543ecaf
josercc <josercc@163.com>
2026-01-20 16:05:17 +0800

    refactor: 替换实时输出进程执行方法以简化代码
    将UnityAarCommand和UnityFrameworkCommand中的runProcessStreamingChecked方法替换为runProcessChecked，简化了进程执行逻辑。这一更改有助于提高代码的可读性和维护性，同时保持功能一致性。

--- lib/commands/build/aar/unity_aar_command.dart
+++ lib/commands/build/aar/unity_aar_command.dart
@@ -136,7 +136,7 @@ class UnityAarCommand extends BuildCacheCommand {
     if (!gradlew.existsSync()) {
       throw Exception('gradlew文件不存在: ${gradlew.path}');
     }
-    await runProcessStreamingChecked(
+    await runProcessChecked(
       [gradlew.absolute.path, 'unityLibrary:bundleReleaseAar'],
       workingDirectory: appHomeDir.androidDir,
     );
