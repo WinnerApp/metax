36c39aee1365eaea78dd18175f7d36149d5aeb01
josercc <josercc@163.com>
2026-03-12 17:21:10 +0800

    fix: 更新metax.dart以添加版本信息并调整命令顺序
    在metax.dart中，添加了版本信息到CommandRunner的描述中，并调整了命令的添加顺序，以提升代码的可读性和一致性。这一改动有助于用户更清晰地了解工具的版本信息。

--- bin/metax.dart
+++ bin/metax.dart
@@ -5,8 +5,8 @@ import 'package:args/command_runner.dart';
 import 'package:meta_tool/app_home_dir.dart';
 import 'package:meta_tool/commands/build/build_command.dart';
 import 'package:meta_tool/commands/cache/cache_command.dart';
-import 'package:meta_tool/commands/init/init.dart';
 import 'package:meta_tool/commands/first_package/first_package_command.dart';
+import 'package:meta_tool/commands/init/init.dart';
 import 'package:meta_tool/commands/publish/publish_command.dart';
 import 'package:meta_tool/commands/test/test_command.dart';
 import 'package:meta_tool/commands/upload/upload_command.dart';
@@ -19,14 +19,14 @@ Future<void> main(List<String> arguments) async {
     stderr.encoding = utf8;
   }
 
-  final runner = CommandRunner('metax', '一款棉宇宙开发和发布工具')
+  final runner = CommandRunner('metax', '一款棉宇宙开发和发布工具（v0.3.5）')
     ..addCommand(InitCommand())
     ..addCommand(BuildCommand())
     ..addCommand(UploadCommand())
     ..addCommand(CacheCommand())
     ..addCommand(PublishCommand())
-      ..addCommand(TestCommand())
-      ..addCommand(FirstPackageCommand());
+    ..addCommand(TestCommand())
+    ..addCommand(FirstPackageCommand());
   runner.argParser.addOption(
     'workspace',
     help: 'app运行目录，默认使用当前目录',
